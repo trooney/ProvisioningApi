0453169305750a3a928d9a6f96fc5e2062e74e80
Tyler Rooney <tyler@tylerrooney.ca>
2012-12-11 14:56:22 -0400

    Fixing test boostrap for new composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$loader = ComposerAutoloaderInit::getLoader();
-
-$loader->add('NRC', __DIR__ );
+$loader = require 'vendor/autoload.php';
+$loader->add('NRC', __DIR__);
